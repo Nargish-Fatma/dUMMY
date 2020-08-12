c8756958ceee2e461113f4107cef5bdeae0daae7
Nargish Fatma <nargishsufi27@gmail.com>
2020-08-12 16:28:09 +0530

    first cmmit message

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("Native Android");
+	}
+	
+	@Test
+	public void IOSApps ()
+	{
+		System.out.println("IOSApps");
+	}
+}
